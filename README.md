2fe815f9bfb63921dced9c2127863a7c91ccbb9e
tobieniepodam <tobieniepodam@gmail.com>
2017-05-31 02:04:55 +0200

    add LICENSE.md file; update README.md; add script arguments

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # letsHttps
+This is the helper bash script for [**acme-tiny**](https://github.com/diafygi/acme-tiny) software - [**Let's Encrypt**](https://letsencrypt.org/) certificates manager.
+
+## How to use
+Example:
+letsHttps.sh -d domain.com -a /var/www/htdocs/domain.com/.well-known/acme-challenge/ -s www -s forum -s www.forum
+
--- letsHttps.sh
+++ letsHttps.sh
@@ -1,7 +1,83 @@
 #!/bin/bash
 
+set -e
+
+pgname=$(basename $0)
 wd=$(dirname $0)
+sslDir='ssl'
+
+help() {
+    echo "Usage:"
+    echo "$pgname -d domain -a pathToAcmeDir [-s subdomain1] [-s subdomain2]"
+    echo
+    echo "Example:"
+    echo "$pgname -d domain.com -p /var/www/htdocs/domain.com/.well-known/acme-challenge/ -s www -s forum -s www.forum"
+    echo
+    exit
+}
+
+
+echo '  _       _    _____  _    _'
+echo ' | | ___ | |_ |  |  || |_ | |_  ___  ___ '
+echo ' | || -_||  _||     ||  _||  _|| . ||_ -|'
+echo ' |_||___||_|  |__|__||_|  |_|  |  _||___|'
+echo '                          |_|'
+echo
+
+
+domain=''
+acmeDir=''
+subdomains=()
+
+while getopts ":d:a:s:h" opt; do
+    case $opt in
+        d)
+            domain="$OPTARG"
+            ;;
+        s)
+            subdomains+=("$OPTARG")
+            ;;
+        a)
+            acmeDir="$OPTARG"
+            ;;
+        h)
+            help
+            exit 0
+            ;;
+        \?)
+            echo -e "Unknown -$OPTARG.\nTry -h for help.\n" >&2
+            exit 1
+            ;;
+        :)
+            echo -e "-$OPTARG require value.\nTry -h for help.\n" >&2
+            exit 1
+            ;;
+    esac
+done
+
+test -z "$domain" && {
+    echo -e "$pgname error: empty domain name.\nTry -h for help.\n" >&2
+    exit 1
+}
+
+test -z "$acmeDir" && {
+    echo -e "$pgname error: empty acme dir.\nTry -h for help.\n" >&2
+    exit 1
+}
+
+echo    "domain     : $domain"
+echo -n "subdomains : "; test ${#subdomains[@]} -gt 0 && echo "${subdomains[@]}" || echo '-'
+echo    "acme dir   : $acmeDir"
+echo
+read -p "continue [y/n]? " -n1 yn
+echo
+test "$yn" == 'y' || exit 0
+
+
 cd $wd
+
+mkdir -p $sslDir
+chmod 700 "$sslDir"
 
 test ! -d 'acme-tiny-master' && {
     echo 'Downoading acme-tiny..'
@@ -12,40 +88,40 @@ test ! -d 'acme-tiny-master' && {
 
 test ! -f 'account.key' && {
     echo 'Generating account.key..'
-    openssl genrsa 4096 > account.key
+    openssl genrsa 4096 > "$sslDir/account.key"
     echo
 }
 
-test -z "$1" && {
-    echo -e "Usage: `basename $0` domain [subdomain1] [subdomain2]\nExample: `basename $0` domain.com www forum www.forum" >&2
-    exit
+domains=""
+test ${#subdomains[@]} -gt 0 && {
+    for subdomain in "${subdomains[@]}"; do
+        domains="$domains,DNS:$subdomain.$domain"
+    done
+    domains="DNS:$domain$domains"
 }
 
-
-domain="$1"
-domains=""
-for subdomain in "${@:2}"; do
-    domains="DNS:$subdomain,$domains"
-done
-test -n "$domains" && domains="DNS:$domain,$domains"
-
-
-
-acmeDir=~/domains/$domain/public_html/.well-known/acme-challenge/
-echo "Creating challenge dir $acmeDir (remove it manualy).."
-mkdir -p $acmeDir
+test ! -d "$acmeDir" && {
+    echo "Creating challenge dir '$acmeDir' (remove it manualy).."
+    mkdir -p "$acmeDir"
+}
 
 echo 'Generating private key..'
-openssl genrsa 4096 > $domain.key
+openssl genrsa 4096 > "$sslDir/$domain.key"
 
 echo 'Generating certificate signing request..'
 test -n "$domains" && {
-    openssl req -new -sha256 -key $domain.key -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=$domains")) > $domain.csr
+    openssl req -new -sha256 -key "$sslDir/$domain.key" -subj "/" -reqexts SAN -config <(cat /etc/ssl/openssl.cnf <(printf "[SAN]\nsubjectAltName=$domains")) > "$sslDir/$domain.csr"
 } || {
-    openssl req -new -sha256 -key $domain.key -subj "/CN=$domain" > $domain.csr
+    openssl req -new -sha256 -key "$sslDir/$domain.key" -subj "/CN=$domain" > "$sslDir/$domain.csr"
 }
 echo
 
 echo 'Generating certificate request..'
-python acme-tiny-master/acme_tiny.py --account-key account.key --csr $domain.csr --acme-dir $acmeDir > $domain.crt
+python acme-tiny-master/acme_tiny.py --account-key account.key --csr "$sslDir/$domain.csr" --acme-dir "$acmeDir" > "$sslDir/$domain.crt"
 
+echo
+echo "Output files in '$sslDir':"
+ls "$sslDir" | grep "$domain"
+
+echo
+echo 'Bye!!'
